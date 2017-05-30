68dff1024ca5268a2c554fb90cdbb305d40d8745
Shishir Amin <samin@pivotal.io>
2017-05-30 14:56:04 -0400

    Create a second creds resource for external director

--- pipeline/tasks/install-node-exporter.sh
+++ pipeline/tasks/install-node-exporter.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-if [[ -s om-bosh-creds/bosh-ca.pem ]]; then
-  bosh -n --ca-cert om-bosh-creds/bosh-ca.pem target `cat om-bosh-creds/director_ip`
+if [[ -s ert-bosh-creds/bosh-ca.pem ]]; then
+  bosh -n --ca-cert ert-bosh-creds/bosh-ca.pem target `cat ert-bosh-creds/director_ip`
 else
-  bosh -n target `cat om-bosh-creds/director_ip`
+  bosh -n target `cat ert-bosh-creds/director_ip`
 fi
 
-BOSH_USERNAME=$(cat om-bosh-creds/bosh-username)
-BOSH_PASSWORD=$(cat om-bosh-creds/bosh-pass)
+BOSH_USERNAME=$(cat ert-bosh-creds/bosh-username)
+BOSH_PASSWORD=$(cat ert-bosh-creds/bosh-pass)
 
 echo "Logging in to BOSH..."
 bosh login <<EOF 1>/dev/null
--- pipeline/tasks/upload-release.sh
+++ pipeline/tasks/upload-release.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-if [[ -s om-bosh-creds/bosh-ca.pem ]]; then
-  bosh -n --ca-cert om-bosh-creds/bosh-ca.pem target `cat om-bosh-creds/director_ip`
+if [[ -s deploy-bosh-creds/bosh-ca.pem ]]; then
+  bosh -n --ca-cert deploy-bosh-creds/bosh-ca.pem target `cat deploy-bosh-creds/director_ip`
 else
-  bosh -n target `cat om-bosh-creds/director_ip`
+  bosh -n target `cat deploy-bosh-creds/director_ip`
 fi
 
-BOSH_USERNAME=$(cat om-bosh-creds/bosh-username)
-BOSH_PASSWORD=$(cat om-bosh-creds/bosh-pass)
+BOSH_USERNAME=$(cat deploy-bosh-creds/bosh-username)
+BOSH_PASSWORD=$(cat deploy-bosh-creds/bosh-pass)
 
 echo "Logging in to BOSH..."
 bosh login <<EOF 1>/dev/null
@@ -19,8 +19,23 @@ EOF
 echo "Uploading Prometheus Release..."
 bosh -n upload release prometheus-release/prometheus-*.tgz
 
-echo "Uploading Node exporter Release..."
-bosh -n upload release node-exporter-release/node-exporter-*.tgz
-
 echo "Uploading Prometheus Customizations Release..."
 bosh -n upload release prometheus-custom-release/prometheus-custom-*.tgz
+
+if [[ -s ert-bosh-creds/bosh-ca.pem ]]; then
+  bosh -n --ca-cert ert-bosh-creds/bosh-ca.pem target `cat ert-bosh-creds/director_ip`
+else
+  bosh -n target `cat ert-bosh-creds/director_ip`
+fi
+
+BOSH_USERNAME=$(cat ert-bosh-creds/bosh-username)
+BOSH_PASSWORD=$(cat ert-bosh-creds/bosh-pass)
+
+echo "Logging in to BOSH..."
+bosh login <<EOF 1>/dev/null
+$BOSH_USERNAME
+$BOSH_PASSWORD
+EOF
+
+echo "Uploading Node exporter Release..."
+bosh -n upload release node-exporter-release/node-exporter-*.tgz
